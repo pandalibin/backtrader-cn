e83f6823f41a2466a710bf25d8a55f6c69faa6da
pandalibin <23249735@qq.com>
2017-10-07 23:17:03 +0800

    Modify conf.py to remove the support for markdown

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from recommonmark.parser import CommonMarkParser
+# from recommonmark.parser import CommonMarkParser
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+# source_parsers = {
+#     '.md': CommonMarkParser,
+# }
 
 # -- General configuration ------------------------------------------------
 
@@ -43,8 +43,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-# source_suffix = '.rst'
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
