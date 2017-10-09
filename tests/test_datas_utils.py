1b5cc0680f15c5925f61495953094ac50a4e8a01
pandalibin <23249735@qq.com>
2017-10-09 11:51:18 +0800

    Add the first unittest case

---
+++ tests/test_datas_utils.py
@@ -0,0 +1,24 @@
+import unittest
+import backtradercn.datas.utils as bdu
+import pandas as pd
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_strip_unused_cols(self):
+        data = pd.DataFrame({
+            'name': ['tom', 'jack'],
+            'age': [24, 56],
+            'gender': ['male', 'male'],
+            'address': ['cn', 'us']
+        })
+        data.index = pd.date_range(start='2017-01-01', periods=2)
+
+        origin_cols = ['name', 'age', 'gender', 'address']
+        unused_cols = ['address']
+        new_cols = ['name', 'age', 'gender']
+
+        self.assertEqual(list(data.columns).sort(), origin_cols.sort())
+
+        bdu.Utils.strip_unused_cols(data, *unused_cols)
+
+        self.assertEqual(list(data.columns).sort(), new_cols.sort())
