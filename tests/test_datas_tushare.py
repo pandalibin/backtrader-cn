9ac0eb3fd904d7a925025abd828badd7ce4a0417
pandalibin <23249735@qq.com>
2017-10-10 09:29:55 +0800

    Fixed #1, add the unitcase for datas/tushare module

--- tests/test_datas_tushare.py
+++ tests/test_datas_tushare.py
@@ -104,4 +104,40 @@ class TsHisDataTestCase(unittest.TestCase):
         self.assertEqual(len(hist_data_000651), 3)
         self.assertEqual(len(hist_data_600085), 3)
 
+    @um.patch('tushare.get_hist_data')
+    def test_download_delta_data_no_data(self, mock_get_hist_data):
+        mock_hist_data = pd.DataFrame(data={
+            'open': [10, 11],
+            'high': [12, 13],
+            'close': [14, 15],
+            'low': [16, 17],
+            'volume': [18, 19],
+            'price_change': [20, 21],
+            'p_change': [22, 23],
+            'ma5': [24, 25],
+            'ma10': [26, 27],
+            'ma20': [28, 29],
+            'v_ma5': [30, 31],
+            'v_ma10': [32, 33],
+            'v_ma20': [34, 35],
+            'turnover': [36, 37]
+        }, index=['2017-01-01', '2017-01-02'])
 
+        mock_get_hist_data.return_value = mock_hist_data
+
+        coll_names = ['000651', '600085']
+        ts_his_data = bdt.TsHisData('localhost', 'ts_hist_lib', *coll_names)
+
+        ts_his_data.init_data()
+
+        mock_delta_data = pd.DataFrame()
+
+        mock_get_hist_data.return_value = mock_delta_data
+
+        ts_his_data.download_delta_data()
+
+        hist_data_000651 = ts_his_data.get_data('000651')
+        hist_data_600085 = ts_his_data.get_data('600085')
+
+        self.assertEqual(len(hist_data_000651), 2)
+        self.assertEqual(len(hist_data_600085), 2)
