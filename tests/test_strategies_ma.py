759b1ecc1652315036d00b4c22486fe87e24a59f
pandalibin <23249735@qq.com>
2017-11-04 17:54:09 +0800

    Update the unit test

--- tests/test_strategies_ma.py
+++ tests/test_strategies_ma.py
@@ -11,6 +11,6 @@ class MATrendStrategyTestCase(unittest.TestCase):
 
     def _test_get_params_list(self):
         training_data = pd.DataFrame(np.random.rand(10, 2))
-        params_list = bsm.MATrendStrategy.get_params_list(training_data)
+        params_list = bsm.MATrendStrategy.get_params_list(training_data, '000651')
 
         self.assertEqual(len(params_list), 4)
