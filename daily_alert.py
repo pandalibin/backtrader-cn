4cac07be46daf2e56d2140a2b884f1b169a66a73
ox0spy <ox0spy@gmail.com>
2017-11-05 08:41:49 +0800

    send daily alert even if do not have market signal.

--- daily_alert.py
+++ daily_alert.py
@@ -11,33 +11,36 @@ logger = logging.getLogger(__name__)
 
 
 def send_daily_alert():
-    store = arctic.Arctic(conf.MONGO_HOST)
-    lib_name = conf.DAILY_STOCK_ALERT_LIBNAME
-    if lib_name not in store.list_libraries():
-        logger.warning(f'can not find library: {lib_name} in Arctic')
-        return None
-    lib = store[lib_name]
-
-    res = {
+    msg = {
         'buy': [],
         'sell': [],
     }
-    symbol = dt.datetime.now().strftime('%Y-%m-%d')
-    df = lib.read(symbol).data
-    data = df.to_dict('records')
-    for item in data:
-        if item['action'] == 'buy':
-            res['buy'].append(item['stock'])
-        elif item['action'] == 'sell':
-            res['sell'].append(item['stock'])
 
+    store = arctic.Arctic(conf.MONGO_HOST)
+    lib_name = conf.DAILY_STOCK_ALERT_LIBNAME
+
+    if lib_name not in store.list_libraries():
+        logger.warning(f'can not find library: {lib_name} in Arctic')
+    else:
+        lib = store[lib_name]
+
+        symbol = dt.datetime.now().strftime('%Y-%m-%d')
+        df = lib.read(symbol).data
+        data = df.to_dict('records')
+        for item in data:
+            if item['action'] == 'buy':
+                msg['buy'].append(item['stock'])
+            elif item['action'] == 'sell':
+                msg['sell'].append(item['stock'])
+
+    # send notification via wechat
     wx_client = WeChatClient({
         'APP_ID': conf.WECHAT_APP_ID,
         'APP_SECRET': conf.WECHAT_APP_SECRET,
     })
 
     response = wx_client.send_all_text_message(
-        json.dumps(res, ensure_ascii=False))
+        json.dumps(msg, ensure_ascii=False))
     logger.debug(response)
 
 
