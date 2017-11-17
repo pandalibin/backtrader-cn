71b9e8c5a846fb04b1e1ea332d545f6598d5cb18
Junkman <ox0spy@gmail.com>
2017-11-17 11:13:53 +0800

    Override XueQiu account info with environment variables.

--- backtradercn/settings/common.py
+++ backtradercn/settings/common.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 
 PROJECT_NAME = 'backtradercn'
 
@@ -17,9 +18,9 @@ WECHAT_APP_ID = 'wx5e8e3c4779887f32'
 WECHAT_APP_SECRET = 'd4624c36b6795d1d99dcf0547af5443d'
 
 # xueqiu account
-XQ_ACCOUNT = '18628391725'
-XQ_PASSWORD = 'gupiao888'
-XQ_PORTFOLIO_MARKET = 'cn'
+XQ_ACCOUNT = os.getenv('XQ_ACCOUNT', '18628391725')
+XQ_PASSWORD = os.getenv('XQ_PASSWORD', 'gupiao888')
+XQ_PORTFOLIO_MARKET = os.getenv('XQ_PORTFOLIO_MARKET', 'cn')
 # 默认的组合前缀，组合名称格式为 组合前缀 + 股票代码
 # 组合名字
 XQ_CUBES_PREFIX = 'SC'
