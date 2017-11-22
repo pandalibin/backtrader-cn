19071bc326d55d80361910a6fc9e98528e1ec216
Junkman <ox0spy@gmail.com>
2017-11-22 12:44:48 +0800

    remove multiprocessing from train_main.py

--- train_main.py
+++ train_main.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-import multiprocessing
-from multiprocessing import Process, Lock
-
 import backtradercn.strategies.ma as bsm
 import backtradercn.tasks as btasks
 from backtradercn.libs.log import getLogger
@@ -12,11 +9,10 @@ from backtradercn.libs import models
 logger = getLogger(__name__)
 
 
-def train(stock, lock):
+def train(stock):
     """
     Run training tasks via multiprocessing and save training params to arctic store.
     :param stock: str, stock code
-    :param lock: the instance of multiprocessing.Lock
     :return: None
     """
 
@@ -24,47 +20,26 @@ def train(stock, lock):
     params = task.train()
     # write stock params to MongoDB
     symbol = conf.STRATEGY_PARAMS_MA_SYMBOL
-    with lock:
-        models.save_training_params(symbol, params)
+    models.save_training_params(symbol, params)
 
 
-def main():
+def main(stock_pools):
     """
     Get all stocks and train params for each stock.
+    :param stock_pools: list, the stock code list.
     :return: None
     """
 
-    stock_pools = models.get_cn_stocks()
-    lock = Lock()
-    processes = multiprocessing.cpu_count()
-    # run subprocess in parallel, the number of processes is: `processes`
-    for i in range(len(stock_pools) // processes + 1):
-        chunk_start = i * processes
-        chunk_end = (i + 1) * processes
-        chunk_lst = stock_pools[chunk_start:chunk_end]
-        if not chunk_lst:
-            break
-
-        logger.debug(f'back test the chunk list: {chunk_lst}')
-        procs = []
-        for stock in chunk_lst:
-            proc = Process(target=train, args=(stock, lock))
-            procs.append(proc)
-            proc.start()
-        for proc in procs:
-            proc.join()
+    for stock in stock_pools:
+        train(stock)
 
 
 if __name__ == '__main__':
-    # drop params library, then re-create it with new data
-    models.drop_library(conf.STRATEGY_PARAMS_LIBNAME)
-    # create empty params library
-    models.create_library(conf.STRATEGY_PARAMS_LIBNAME)
+    # create params library if not exist
+    models.get_or_create_library(conf.STRATEGY_PARAMS_LIBNAME)
 
-    # train('000651')
-    # train('000001')
-
-    main()
+    cn_stocks = models.get_cn_stocks()
+    main(cn_stocks)
 
     # training 时会写数据到 `conf.DAILY_STOCK_ALERT_LIBNAME`
     models.drop_library(conf.DAILY_STOCK_ALERT_LIBNAME)
